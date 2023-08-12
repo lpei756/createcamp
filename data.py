696abb4426dd2c22cc1e62340bd20cfc5d6816c6
nerkamyildirim <nerkamyildirim@gmail.com>
2023-08-13 11:22:34 +1200

    Added 'categories' to repo

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ with open('location_data.json', 'r') as json_file:
 
 category_list = []
 for i in range(len(sample_load_file['results'])):
-	category_list.append(sample_load_file['results'][i]['name'])
+	category_list.append(sample_load_file['results'][i]['types'][0])
 		
 # Extract 'location' field
 # location = sample_load_file['results'][0]['geometry']['location']
@@ -16,9 +16,9 @@ for i in range(len(sample_load_file['results'])):
 print(category_list)
 
 # Define the filename for the new JSON file
-new_file_name = "place_name.json"
+new_file_name = "categories.json"
 
 # Write the list to the new JSON file
 with open(new_file_name, 'w') as json_file:
-    json.dump(place_name_list, json_file, indent=4)
+    json.dump(category_list, json_file, indent=4)
 
